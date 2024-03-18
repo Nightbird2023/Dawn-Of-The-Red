9a1cc8c5566c6e17e122b4479b864bf8689f73a3
BensoneWhite <145090119+BensoneWhite@users.noreply.github.com>
2024-03-17 22:59:21 -0700

    Setting up the mod

---
+++ src/Global.cs
@@ -0,0 +1,31 @@
+﻿global using System;
+global using System.IO;
+global using System.Security.Permissions;
+global using System.Collections.Generic;
+global using System.Linq;
+global using RWCustom;
+global using UnityEngine;
+global using Mono.Cecil.Cil;
+global using MonoMod.Cil;
+global using Random = UnityEngine.Random;
+global using BepInEx;
+global using DevInterface;
+global using CreatureType = CreatureTemplate.Type;
+global using static PathCost.Legality;
+global using Fisobs.Core;
+global using Fisobs.Creatures;
+global using Fisobs.Sandbox;
+global using Fisobs.Items;
+global using Fisobs.Properties;
+global using SlugBase.DataTypes;
+global using System.Runtime.CompilerServices;
+global using static Player;
+global using DressMySlugcat;
+global using static MoreSlugcats.SingularityBomb;
+global using System.Reflection;
+global using MoreSlugcats;
+global using Menu.Remix.MixedUI;
+global using HUD;
+global using VoidSea;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
---
+++ src/Plugin.cs
@@ -0,0 +1,68 @@
+﻿namespace DawnOfTheRed;
+
+[BepInPlugin(GUID: AUTHORS, Name: MOD_NAME, Version: VERSION)]
+class Plugin : BaseUnityPlugin
+{
+    public const string AUTHORS = "BensoneWhite";
+    public const string MOD_NAME = "dawnofthered";
+    public const string VERSION = "0.0.1";
+
+    public bool IsInit;
+    public bool IsPreInit;
+    public bool IsPostInit;
+
+    public void OnEnable()
+    {
+        On.RainWorld.PreModsInit += RainWorld_PreModsInit;
+        On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+        On.RainWorld.PostModsInit += RainWorld_PostModsInit;
+
+        Debug.LogWarning($"{MOD_NAME} is loading... {VERSION}");
+    }
+
+    private void RainWorld_PostModsInit(On.RainWorld.orig_PostModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        try
+        {
+            if (IsPostInit) return;
+            IsPostInit = true;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    private void RainWorld_PreModsInit(On.RainWorld.orig_PreModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        try
+        {
+            if (IsPreInit) return;
+            IsPreInit = true;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+
+        try
+        {
+            if (IsInit) return;
+            IsInit = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            Debug.LogError(e);
+        }
+    }
+}
